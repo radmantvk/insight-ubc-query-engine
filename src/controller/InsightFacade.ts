c8b795dc5ed0c5bc8545226de021f7a382f994f6
Parham Alijani <palijani@students.cs.ubc.ca>
2021-10-03 16:07:39 -0700

    1. modified validity methods, 2. added a JSONValidifier method, 3. Worked on unzip method

--- src/controller/InsightFacade.ts
+++ src/controller/InsightFacade.ts
@@ -1,4 +1,5 @@
 import {IInsightFacade, InsightDataset, InsightDatasetKind, InsightError, NotFoundError} from "./IInsightFacade";
+import * as fs from "fs-extra";
 
 /**
  * This is the main programmatic entry point for the project.
@@ -7,12 +8,17 @@ import {IInsightFacade, InsightDataset, InsightDatasetKind, InsightError, NotFou
  */
 export default class InsightFacade implements IInsightFacade {
 	private dataSets: string[] = [];
+
 	constructor() {
 		console.trace("InsightFacadeImpl::init()");
 	}
 
 	public addDataset(id: string, content: string, kind: InsightDatasetKind): Promise<string[]> {
-		this.checkIDandKindValidity(id, kind)
+		this.checkIDValidity(id)
+			.catch((err) => {
+				return err;
+			});
+		this.checkKindValidity(kind)
 			.catch((err) => {
 				return err;
 			});
@@ -28,7 +34,12 @@ export default class InsightFacade implements IInsightFacade {
 	}
 
 	public removeDataset(id: string): Promise<string> {
-		return Promise.resolve("Not implemented.");
+		this.checkIDValidity(id)
+			.catch((err) => {
+				return err;
+			});
+		delete this.dataSets[this.dataSets.indexOf(id)];
+		return Promise.resolve(id);
 	}
 
 	public performQuery(query: any): Promise<any[]> {
@@ -40,15 +51,25 @@ export default class InsightFacade implements IInsightFacade {
 	}
 
 	/**
+	 * Checks the kind of dataset validity
+	 * @param kind  the kind of dataset (room or courses)
+	 * @return Promise <string[]>
+	 */
+	private checkKindValidity(kind: InsightDatasetKind): Promise<InsightDatasetKind> {
+		if (kind === InsightDatasetKind.Rooms) {
+			console.log("InsightDatasetKind is Rooms");
+			return Promise.reject(InsightError);
+		}
+		return Promise.resolve(kind);
+	}
+
+	/**
 	 * Checks ID's validity
 	 * @param id  The id of the dataset being added.
 	 * @return Promise <string[]>
 	 */
-	private checkIDandKindValidity(id: string, kind: InsightDatasetKind) {
-		if (kind === InsightDatasetKind.Rooms){
-			console.log("InsightDatasetKind is Rooms");
-			return Promise.reject(InsightError);
-		} else if (this.dataSets.includes(id)) {
+	private checkIDValidity(id: string): Promise<string> {
+		if (this.dataSets.includes(id)) {
 			console.log("id was already added");
 			return Promise.reject(InsightError);
 		} else if (this.dataSets.includes("_")) {
@@ -63,6 +84,34 @@ export default class InsightFacade implements IInsightFacade {
 		return Promise.resolve(id);
 	}
 
+	private unzip(content: string) {
+		const JSZip = require("jszip");
+		const zip = new JSZip();
+		const path = "project_team147/data/courses/";
+		zip.loadAsync(content, {base64: true})
+			.then(function (contents: any) {
+				Object.keys(zip.files).forEach(function (name) {
+					let data = zip.files[name];							// each course file inside dataset stored here
+					let location = path + name;							// location that we want to store the course file
+					fs.writeFileSync(location, data);					// write to file at the designated location with designated course
+				});
+			});
+	}
+
+
+	/**
+	 * Parsing a string to json format and checking if it is valid
+	 */
+	// TODO: figure out how we would access the content inside a single json file
+	private isJsonString(str: string) {
+		try {
+			JSON.parse(str);
+		} catch (error) {
+			return false;
+		}
+		return true;
+	}
+}
 	// private unzip(content:string) {
 	//     // const JSZip = require("jszip");
 	//     // const fs = require("fs-extra");
@@ -91,4 +140,4 @@ export default class InsightFacade implements IInsightFacade {
 	//     //
 	//     // });
 	// }
-}
+
