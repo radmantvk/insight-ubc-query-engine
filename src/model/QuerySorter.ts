55e054522e9f5a085b6c8441bdc994bd598e39d5
Radman Avakoli <raditvk@gmail.com>
2021-11-17 00:04:57 -0800

    backup 2

--- src/model/QuerySorter.ts
+++ src/model/QuerySorter.ts
@@ -34,20 +34,20 @@ export default class Sorter {
 		if (order === "avg" || order === "pass" || order === "fail" || order === "audit" || order === "year" ||
 			order === "lat" || order === "lon" || order === "seats") {
 			secOrRoomList.sort((a: any, b: any) => {
-				return this.sortFunction(finishedOrders, a, b, order);
+				return this.sortFunction(finishedOrders, a, b, order, true);
 			});
 		} else if (order === "dept" || order === "id" || order === "instructor" || order === "title" ||
 			order === "uuid" ||	order === "fullname" || order === "shortname" || order === "number" ||
 			order === "name" || order === "address" || order === "type" || order === "furniture" ||
 			order === "href"){
 			secOrRoomList.sort((a: Section, b: Section) => {
-				return this.sortFunction(finishedOrders, a, b, order);
+				return this.sortFunction(finishedOrders, a, b, order, false);
 			});
 		}
 		return secOrRoomList;
 	}
 
-	private sortFunction(finishedOrders: string[], a: Section, b: Section, order: string) {
+	private sortFunction(finishedOrders: string[], a: Section, b: Section, order: string, isMField: boolean) {
 		let isSortable = true;
 		for (const ord of finishedOrders) {
 			if (FieldAccessor.getField(ord, a) !== FieldAccessor.getField(ord, b)) {
@@ -56,7 +56,12 @@ export default class Sorter {
 			}
 		}
 		if (isSortable) {
-			return FieldAccessor.getField(order, a) - FieldAccessor.getField(order, b);
+			if (isMField) {
+				return FieldAccessor.getField(order, a) - FieldAccessor.getField(order, b).toString();
+			} else {
+				return FieldAccessor.getField(order, a).toString()
+					.localeCompare(FieldAccessor.getField(order, b).toString());
+			}
 		} else {
 			return 0;
 		}
